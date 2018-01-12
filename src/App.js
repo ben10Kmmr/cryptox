b8de6a1f54499783187f9a82250b5235c5a6fded
ben10Kmmr <ntnt3844@gmail.com>
2018-01-12 16:41:39 +0700

    refactor

--- src/App.js
+++ src/App.js
@@ -4,31 +4,42 @@ import axios from 'axios'
 import _ from 'lodash'
 
 class App extends Component {
-  Symbols = ['OMG','BTC','XRP']
-  
+  SYMBOLS = ['OMG', 'BTC', 'XRP']
+
   constructor(props){
+    super(props)
+    this.state = {}
   }
 
-  componentDidMount(){
+  //refactor
+  extractPrice(data){
+    let price_state = {}
+    _.each(this.SYMBOLS, symbol => {
+      price_state[symbol] = _.find(data, ['symbol', symbol]).price_thb
+    })
+    this.setState(price_state)
+  }
+
+  loadPrice(){
     let self = this
     axios.get('https://api.coinmarketcap.com/v1/ticker/?convert=THB')
     .then(function (response) {
-      let price_state = {}
-      _.each(self.Symbols,symbol => {
-        price_state[symbol] = _.find(response.data, ['symbol',symbol]).price_state
+      self.extractPrice(response.data)
     })
-    self.setState(price_state)
-  })
     .catch(function (error) {
       console.log(error);
     });
+  }
+
+  componentDidMount(){
+    setInterval(this.loadPrice.bind(this), 3000)
   }
 
   render() {
     return (
       <div>
         {
-          _.map(this.Symbols, symbol => <Ticker symbol={symbol} price={this.state[symbol]}/>)
+          _.map(this.SYMBOLS, symbol => <Ticker symbol={symbol} price={this.state[symbol]}/>)
         }
       </div>
     );
